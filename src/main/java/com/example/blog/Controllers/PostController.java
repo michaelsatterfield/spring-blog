14e8c4077abdb02236dd091c77b68b1d226e15b2
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-11-24 11:45:30 -0600

    fixed redundant code

--- src/main/java/com/example/blog/Controllers/PostController.java
+++ src/main/java/com/example/blog/Controllers/PostController.java
@@ -1,4 +1,33 @@
 package com.example.blog.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class PostController {
+    @GetMapping ("/posts")
+    @ResponseBody
+    public String index() {
+        return "This is the Post Index Page.";
+    }
+
+    @GetMapping ("/posts/{id}")
+    @ResponseBody
+    public String viewPost(@PathVariable long id) {
+        return "This is the View Post Page : "+ id + ".";
+    }
+    @GetMapping ("/posts/create")
+    @ResponseBody
+    public String viewCreatePost() {
+        return "This is a View Create Post Page.";
+    }
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost() {
+        return "This is a Create Post Page.";
+    }
+
+
+
+
 }
