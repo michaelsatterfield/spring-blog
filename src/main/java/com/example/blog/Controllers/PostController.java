8a85922bca43cc87cd608dbd0a521b9d700ecb7f
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-11-27 23:23:34 -0600

    finished exercises and added styling

--- src/main/java/com/example/blog/Controllers/PostController.java
+++ src/main/java/com/example/blog/Controllers/PostController.java
@@ -1,15 +1,33 @@
 package com.example.blog.Controllers;
 
+import com.example.blog.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Controller
 public class PostController {
-    @GetMapping ("/posts")
-    @ResponseBody
-    public String index() {
-        return "This is the Post Index Page.";
+    @GetMapping ("/posts/index")
+    public String allPosts(Model model) {
+        List<String> newPosts = new ArrayList<>();
+        newPosts.add("Telephone for Sale, 25 Dollars.");
+        newPosts.add("Rechargeable Batteries in Box, 10 Dollars");
+
+        model.addAttribute("newPosts",newPosts );
+        return "posts/index";
     }
+
+    @GetMapping ("/posts/show")
+    public String individualPost(@ModelAttribute String post, Model model) {
+        model.addAttribute("post"," Here is a New Post!" + post );
+        //        Post newPost = new Post();
+        return "/posts/show";
+    };
 
     @GetMapping ("/posts/{id}")
     @ResponseBody
@@ -26,6 +44,8 @@ public class PostController {
     public String createPost() {
         return "This is a Create Post Page.";
     }
+
+
 
 
 
---
+++ src/main/java/com/example/blog/Post.java
@@ -0,0 +1,14 @@
+package com.example.blog;
+
+public class Post {
+    private String title;
+    private String body;
+
+    public String getTitle() { return title; }
+
+    public String getBody() { return body; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public void setBody(String body) { this.body = body; }
+}
