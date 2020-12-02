b4e768e32f64c096dd8ed67165148fff78b93706
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-12-02 13:01:36 -0600

    fixed crud

--- src/main/java/com/example/blog/Controllers/PostController.java
+++ src/main/java/com/example/blog/Controllers/PostController.java
@@ -18,48 +18,32 @@ public class PostController {
     public PostController(PostRepository postDao){
         this.postDao = postDao;
     }
-
+//Show all posts
     @GetMapping("/posts")
     public String index(Model viewModel) {
         viewModel.addAttribute("posts", postDao.findAll());
         return "posts/index";
     }
 
-
-
-    @GetMapping ("/posts/index")
-    public String allPosts(Model model) {
-        List<String> newPosts = new ArrayList<>();
-        newPosts.add("Telephone for Sale, 25 Dollars.");
-        newPosts.add("Rechargeable Batteries in Box, 10 Dollars");
-
-        model.addAttribute("newPosts",newPosts );
-        return "posts/index";
-    }
-
-    @GetMapping ("/posts/show")
-    public String individualPost(@ModelAttribute String post, Model model) {
-
-        model.addAttribute("post"," Here is a New Post!" + post );
-        //        Post newPost = new Post();
-        return "/posts/show";
-    };
-
-    @GetMapping ("/posts/{id}")
-    @ResponseBody
-    public String viewPost(@PathVariable long id) {
-        return "This is the View Post Page : "+ id + ".";
-    }
-    @GetMapping ("/posts/create")
-    public String viewCreateForm(Model model) {
-        model.addAttribute("posts/create", new Post());
-        return "posts/create";
-    }
-//    @PostMapping("/posts/create")
-//    @ResponseBody
-//    public String createPost() {
-//        return "This is a Create Post Page.";
+//    @GetMapping ("/posts/search")
+//    public String search(@RequestParam(name = "term") String term, Model viewModel){
+//        term = "%"+term+"%";
+//        List<Post> dbPosts = postDao.findOne(term);
+//        viewModel.addAttribute("ads", dbPosts);
+//        return "ads/index";
 //    }
+
+//edit this method
+    @GetMapping ("/posts/{id}")
+    public String show(@PathVariable long id, Model viewModel) {
+        viewModel.addAttribute("post", postDao.getOne(id));
+        return "posts/show";
+    }
+
+    @GetMapping ("/posts/create")
+    public String viewCreateForm() {
+        return "posts/new";
+    }
 
     @PostMapping("/posts/create")
 //    @ResponseBody
@@ -67,12 +51,36 @@ public class PostController {
             @RequestParam(name = "title") String title,
             @RequestParam(name = "body") String body
     ){
-        Post post = new Post(title,body);
-        Post dbPost = postDao.save(post);
-        return dbPost.getId() + dbPost.getTitle() + dbPost.getBody();
+        Post post = new Post(title, body);
+        postDao.save(post);
+        return "redirect:/posts";
     }
 
+    @GetMapping("/posts/{id}/edit")
+    public String showEditPost(@PathVariable long id, Model viewModel){
+        viewModel.addAttribute("post", postDao.getOne(id));
+        return "posts/edit";
+    }
 
+    @PostMapping("/posts/{id}/edit")
+//    @ResponseBody
+    public String editPost(
+            @PathVariable long id,
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "body") String body
+    ){
+        Post dbPost = postDao.getOne(id);
+        dbPost.setTitle(title);
+        dbPost.setBody(body);
+        postDao.save(dbPost);
+        return "redirect:/posts/" +dbPost.getId();
+    }
+ @PostMapping("posts/{id}/delete")
+    public String deleteAd(@PathVariable long id){
+        postDao.deleteById(id);
+        return "redirect:/posts";
+ }
+//     Delete Method
 
 
 
